0727e8995fccf2d316933c02ae0b9c061e69f3e0
shouchao.shi <stephenAlan@163.com>
2020-12-06 10:19:16 +0800

    eureka集群两个节点相互注册

---
+++ cloud-eureka-server7002/src/main/java/com/stephen/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.stephen.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by ssc on 2020-12-06 10:09 .
+ * Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
