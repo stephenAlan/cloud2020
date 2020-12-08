d4d9fae8f78094041b05569f6bfe89d1dfca7095
shouchao.shi <stephenAlan@163.com>
2020-12-08 09:59:20 +0800

    TransactionTemplate实现编程式事务

--- cloud-provider-payment8001/src/main/java/com/stephen/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/stephen/springcloud/controller/PaymentController.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -31,18 +34,43 @@ public class PaymentController {
 
     @Resource
     private DiscoveryClient discoveryClient;
+    @Resource
+    private TransactionTemplate transactionTemplate;
 
     @PostMapping("/create")
     public CommonResult create(@RequestBody Payment payment) {
-        int result = paymentService.create(payment);
-        log.info("插入payment结果： " + result);
-        log.info("from server: " + serverPort);
-        if (result > 0) {
+        // int result = paymentService.create(payment);
+        // log.info("插入payment结果： " + result);
+        // log.info("from server: " + serverPort);
+        // int i = 1 / 0;
+        Boolean result = testTransaction(payment);
+        if (result) {
             return ResultUtils.success("server.port:" + serverPort,result);
         } else {
             return ResultUtils.fail();
         }
     }
+
+    public Boolean testTransaction(Payment payment) {
+        Integer result = transactionTemplate.execute(new TransactionCallback<Integer>() {
+            @Override
+            public Integer doInTransaction(TransactionStatus status) {
+                try {
+                    int result = paymentService.create(payment);
+                    log.info("插入payment结果： " + result);
+                    log.info("from server: " + serverPort);
+                    int i = 1 / 0;
+                    return 1;
+                } catch (Exception e) {
+                    status.setRollbackOnly();
+                    log.info(e.getMessage(), e);
+                }
+                return 0;
+            }
+        });
+        return result != null && result > 0 ? true : false;
+    }
+
 
     @GetMapping("/get/{id}")
     public CommonResult getPaymentById(@PathVariable Long id) {
