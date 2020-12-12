aec7ab003ec2dd660110e0fa81b4799a52bdfa92
shouchao.shi <stephenAlan@163.com>
2020-12-12 23:10:37 +0800

    配置全局默认Hystrix fallback方法,普通方法可以通用，特殊方法的才需要特殊配置

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/stephen/springcloud/constroller/ConsumerPaymentController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/stephen/springcloud/constroller/ConsumerPaymentController.java
@@ -1,7 +1,7 @@
 package com.stephen.springcloud.constroller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.stephen.springcloud.service.FeignService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +17,7 @@ import javax.annotation.Resource;
 @RestController
 @Slf4j
 @RequestMapping("consumer")
+@DefaultProperties(defaultFallback = "global_fallback")
 public class ConsumerPaymentController {
 
     @Resource
@@ -27,17 +28,23 @@ public class ConsumerPaymentController {
         return feignService.paymentOk();
     }
 
-    @HystrixCommand(fallbackMethod = "paymentTimeout_fallback",commandProperties = { // 3秒超时
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "6000")
-    })
+    @HystrixCommand
+    // @HystrixCommand(fallbackMethod = "paymentTimeout_fallback",commandProperties = { // 3秒超时
+    //         @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "6000")
+    // })
     @GetMapping("paymentTimeout")
     public String paymentTimeout() throws InterruptedException {
-        // int i = 1 / 0;
+        int i = 1 / 0;
         return  feignService.paymentTimeout() ;
     }
 
     public String paymentTimeout_fallback() throws InterruptedException {
         return "consumer 80 支付系统繁忙，请稍后再试";
     }
+
+    public String global_fallback() throws InterruptedException {
+        return "global_fallback method handler";
+    }
+
 
 }
