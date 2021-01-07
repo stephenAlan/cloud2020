97d4fd1a068909b3d7a2df36017a23dce4f46cd7
shouchao.shi <stephenAlan@163.com>
2021-01-07 09:45:06 +0800

    在config中配置多个路由,在配置文件中模拟同样的效果

--- cloud-gateway-gateway9527/src/main/java/com/stephen/springcloud/config/GateWayConfig.java
+++ cloud-gateway-gateway9527/src/main/java/com/stephen/springcloud/config/GateWayConfig.java
@@ -7,7 +7,8 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by ssc on 2021-01-07 9:29 .
- * Description:
+ * Description: 若需要配置多个路由,就取不同的id名称,不重复即可
+ *  但是这种没有写在配置文件中方便
  */
 @Configuration
 public class GateWayConfig {
@@ -21,6 +22,13 @@ public class GateWayConfig {
     public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
         RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
         routes.route("path_route_custom",r -> r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator1(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_custom1",r -> r.path("/guoji").uri("http://news.baidu.com/guoji")).build();
         return routes.build();
     }
 
